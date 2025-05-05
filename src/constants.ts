2940a23b08265e3071612274c9f1a7232c2a9a63
magae1 <jjilil8351@gmail.com>
2025-05-05 14:52:34 +0900

    🚀 Add environment variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
-export const TALK_SERVER_URL = "http://localhost:8000";
+export const TALK_SERVER_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://p01--fastapi-server--lk9hrnkdwbxn.code.run"
+    : "http://localhost:8000";
 
-export const TALK_WEBSOCKET_URL = "ws://localhost:8000/ws";
+export const TALK_WEBSOCKET_URL =
+  process.env.NODE_ENV === "production"
+    ? "wss://p01--fastapi-server--lk9hrnkdwbxn.code.run/ws"
+    : "ws://localhost:8000/ws";
--- src/repositories/fetchers.ts
+++ src/repositories/fetchers.ts
@@ -4,6 +4,10 @@ import { TALK_SERVER_URL } from "../constants.ts";
 
 const talkFetcher = axios.create({
   baseURL: TALK_SERVER_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export const getIceServers = async (): Promise<RTCIceServer[]> =>
