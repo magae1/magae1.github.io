5dd338f47b1e1108dd72fbe34c488a4053ba2921
magae1 <jjilil8351@gmail.com>
2025-07-30 00:10:46 +0900

    👽️ Change API server URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const TALK_SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? "https://p01--fastapi-server--lk9hrnkdwbxn.code.run"
+    ? "https://magae-talk-server.onrender.com/"
     : "http://localhost:8000";
 
 export const TALK_WEBSOCKET_URL =
   process.env.NODE_ENV === "production"
-    ? "wss://p01--fastapi-server--lk9hrnkdwbxn.code.run/ws"
+    ? "wss://magae-talk-server.onrender.com/ws"
     : "ws://localhost:8000/ws";
