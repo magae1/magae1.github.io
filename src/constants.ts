d584bfeebf35a1bc9c861d07e6286082f7b41507
magae1 <jjilil8351@gmail.com>
2025-08-14 23:27:54 +0900

    🚀 Change api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const TALK_SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? "https://magae-talk-server.onrender.com/"
+    ? "https://scary-rivi-magae-private-351627d5.koyeb.app/"
     : "http://localhost:8000";
 
 export const TALK_WEBSOCKET_URL =
   process.env.NODE_ENV === "production"
-    ? "wss://magae-talk-server.onrender.com/ws"
+    ? "wss://scary-rivi-magae-private-351627d5.koyeb.app/ws"
     : "ws://localhost:8000/ws";
